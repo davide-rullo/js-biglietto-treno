1ba26841212c92d6e79147ba905d5de7896e5795
davide-rullo <davide.rullo01@gmail.com>
2023-08-23 18:06:31 +0200

    fix
    fixed age bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,13 @@ let finalPrice;
 if (age < 18){
     price = price - (price * 20 / 100);
     finalPrice = price.toFixed(2);
-} else if (age >= 65){
-    price = price - (price * 40 / 100).toFixed(2);
+} else if (age >= 18 && age <65){
+    let price;
     finalPrice = price.toFixed(2);
 }
+ else if (age >= 65){
+    price = price - (price * 40 / 100);
+    finalPrice = price.toFixed(2);
+} 
 
 document.getElementById("ticket").innerHTML = "Il prezzo del biglietto è di " + finalPrice + " €"
